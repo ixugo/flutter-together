453b356f1b4d0077bf2db64b08a7ccdd1a066b29
xiexu <xiexu2018@gmail.com>
2021-04-14 09:36:55 +0800

    更改包名

--- android/app/src/main/kotlin/com/example/flutter_together/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_together/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_together
+package com.together.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/api/http.dart
+++ lib/api/http.dart
@@ -1,13 +1,16 @@
-import 'dart:io';
-
-import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_together/api/http_error.dart';
+import 'package:flutter_together/common/log.dart';
 
 class Http {
   ///超时时间
   static const int CONNECT_TIMEOUT = 10000;
   static const int RECEIVE_TIMEOUT = 10000;
+
+  static const String GET = 'get';
+  static const String POST = 'post';
+  static const String DELETE = 'delete';
+  static const String PUT = "put";
 
   static Http _instance = Http._internal();
 
@@ -30,18 +33,18 @@ class Http {
 
       // 添加拦截器
       dio.interceptors.add(ErrorInterceptor());
-      dio.interceptors.add(LogInterceptor());
+      // dio.interceptors.add(LogInterceptor());
 
       // 在调试模式下需要抓包调试，所以我们使用代理，并禁用HTTPS证书校验
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-          (client) {
-        // client.findProxy = (uri) {
-        // return "PROXY $PROXY_IP:$PROXY_PORT";
-        // };
-        //代理工具会提供一个抓包的自签名证书，会通不过证书校验，所以我们禁用证书校验
-        client.badCertificateCallback =
-            (X509Certificate cert, String host, int port) => true;
-      };
+      // (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+      //     (client) {
+      // client.findProxy = (uri) {
+      // return "PROXY $PROXY_IP:$PROXY_PORT";
+      // };
+      //代理工具会提供一个抓包的自签名证书，会通不过证书校验，所以我们禁用证书校验
+      // client.badCertificateCallback =
+      // (X509Certificate cert, String host, int port) => true;
+      // };
     }
   }
 
@@ -70,26 +73,43 @@ class Http {
   Future request(
     String path, {
     data,
+    params,
     Options options,
   }) async {
     // Map<String, dynamic> _authorization = getAuthorizationHeader();
     // if (_authorization != null) {
     // requestOptions = requestOptions.merge(headers: _authorization);
     // }
-    Response response = await dio.request(path, data: data, options: options);
+    //
+    logs.d('请求地址：【 ${dio.options.baseUrl}$path ${options.method} 】');
+
+    logs.d('请求数据：【  $data  】');
+    data = data ?? {};
+
+    Response response;
+    try {
+      response = await dio.request(
+        path,
+        data: data,
+        queryParameters: params,
+        options: options,
+      );
+    } catch (e) {}
+
+    logs.d('获取数据：【  ${response.data}  】');
     return response;
   }
 
   /// restful get 操作
   Future get(
     String path, {
-    Map<String, dynamic> data,
+    Map<String, dynamic> params,
   }) async {
     return await request(
       path,
-      data: data,
+      params: params,
       options: Options(
-        method: "GET",
+        method: GET,
       ),
     );
   }
@@ -104,7 +124,7 @@ class Http {
       data: data,
       options: Options(
         contentType: Headers.jsonContentType,
-        method: "POST",
+        method: POST,
       ),
     );
   }
@@ -119,7 +139,7 @@ class Http {
       data: data,
       options: Options(
         contentType: Headers.jsonContentType,
-        method: "PUT",
+        method: PUT,
       ),
     );
   }
@@ -134,7 +154,7 @@ class Http {
       data: data,
       options: Options(
         contentType: Headers.jsonContentType,
-        method: "DELETE",
+        method: DELETE,
       ),
     );
   }
@@ -149,8 +169,12 @@ class Http {
       path,
       data: FormData.fromMap(data),
       options: Options(
-        method: "POST",
+        method: POST,
       ),
     );
+  }
+
+  Future<Response> getHTML(String url) async {
+    return await Dio().get(url);
   }
 }
