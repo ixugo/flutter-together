24b27454d93416c64e9f8692d5d311636944eaab
xiexu <xiexu2018@gmail.com>
2021-09-16 11:38:31 +0800

    依赖库更新

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_together/app.dart';
 import 'package:flutter_together/common/global.dart';
+import 'package:flutter_together/api/http.dart';
 import 'package:flutter_together/providers/app_state_model.dart';
 import 'package:flutter_together/providers/appbar.dart';
-import 'package:flutter_together/providers/theme.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final providers = [
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel(),
+    ),
+    ChangeNotifierProvider<BottomAppbarModel>(
+      create: (_) => BottomAppbarModel(),
+    ),
+  ];
+
+  Http().init("https://api.golang.space/v1");
   Global.init().then(
     (value) => runApp(
       MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AppStateModel>(
-            create: (_) => AppStateModel(),
-          ),
-          ChangeNotifierProvider<BottomAppbarModel>(
-            create: (_) => BottomAppbarModel(),
-          ),
-          ChangeNotifierProvider<ThemeModel>(
-            create: (_) => ThemeModel(),
-          ),
-        ],
+        providers: providers,
         child: TogetherApp(),
       ),
     ),
--- lib/view/blog/blog.dart
+++ lib/view/blog/blog.dart
@@ -107,6 +107,14 @@ class BlogDetalViewState extends State<BlogDetalView> {
   }
 
   Widget _buildBody(ctx, img) {
+    // if (!widget.article.link.contains("golang.space")) {
+    //   Navigator.push(
+    //     ctx,
+    //     CupertinoPageRoute(builder: (_) => IWebView(widget.article.link)),
+    //   );
+    //   return Container();
+    // }
+
     Widget w = WebPage(widget.article.link);
 
     final double height = 210;
@@ -207,16 +215,6 @@ class OpenContainerWrapper extends StatelessWidget {
       closedElevation: 0, // 关闭阴影
       transitionType: transitionType,
       openBuilder: (BuildContext ctx, VoidCallback _) {
-        if (!article.link.contains("golang.space")) {
-          // launchURL(article.link);
-          // return null;
-          //
-          Navigator.push(
-            ctx,
-            MaterialPageRoute(builder: (_) => IWebView(article.link)),
-          );
-          return null;
-        }
         return BlogDetalView(
           key: Key(article.link),
           article: article,
